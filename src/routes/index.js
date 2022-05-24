6b2a8f81b8a51320dc749fc9a263faa75eeaeea2
hoangat4848 <hoangat4848@gmail.com>
2022-05-24 12:32:10 +0700

    feat(cart): add cart model, controller, views

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
-const siteRouter = require('./site');
-const productsRouter = require('./products');
-
+const siteRouter = require('./site')
+const productsRouter = require('./products')
+const userRouter = require('./user')
+const cartRouter = require('./cart')
+const orderRouter = require('./orders')
+const Product = require('../app/models/Product')
 
 function route(app) {
-  app.use('/products', productsRouter);
-
-  app.use('/', siteRouter);
+  app.use('/products', productsRouter)
+  app.use('/login', userRouter)
+  app.use('/cart', cartRouter)
+  app.use('/order', orderRouter)
+  app.use('/', siteRouter)
 }
-
-module.exports = route;
+module.exports = route
