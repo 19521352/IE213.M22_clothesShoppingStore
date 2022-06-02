b17b00ef087a0eebf2b1afd485eaedb8355956e5
NguyenLocLinh <62052787+LocLinh@users.noreply.github.com>
2022-06-03 00:10:41 +0700

    fix some bug of filter

--- src/app/controllers/FilterController.js
+++ src/app/controllers/FilterController.js
@@ -1,5 +1,5 @@
-const Product = require('../models/Product');
-
+const Product = require('../models/Product')
+const { getPrice } = require('../../util/products/show')
 const { mutipleMongooseToObject } = require('../../util/mongoose');
 const { query } = require('express');
 
@@ -9,16 +9,43 @@ class SiteController {
     filter(req, res, next) {
         const params = req.query;
         // console.log(params);
-        Product.find(params)
-        .then((clothesItems) => {
+        // Product.find(params)
+        // .then((clothesItems) => {
+        //     res.render('home', {
+        //     clothesItems: clothesItems.map(e => Object.assign(e, getPrice(e.skus))),
+        //     user:req.user,
+        //     isLogin: req.user,
+        //     });
+        // })
+        // .catch(next);
+        
+        let productQuery = Product.find(params).lean()
+
+        if (req.query.hasOwnProperty('_sort')) {
+        productQuery = productQuery.sort({
+            [req.query.column]: [req.query.type.split('.')]
+        })
+        }
+        Promise.all([productQuery])
+        .then(([clothesItems]) => {
             res.render('home', {
-            clothesItems: mutipleMongooseToObject(clothesItems),
-            user:req.user,
+            layout: 'main',
+            title: 'Trang chủ',
+            user : req.user,
             isLogin: req.user,
+            clothesItems: clothesItems.map(e => Object.assign(e, getPrice(e.skus)))
+
             });
         })
-        .catch(next);
+        .catch((error) =>
+            res.render('404', {
+            layout: false,
+            title: '404 error',
+            })
+        )
+
     }
+    
 
 }
 
